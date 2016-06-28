bf97cfd30d033b68635315b6066b91f8cc2e3612
Aman Sandhu <ass17@sfu.ca>
2016-06-28 16:54:36 -0700

    WIP

--- retrieve_ops.py
+++ retrieve_ops.py
@@ -5,31 +5,29 @@ from boto.dynamodb2.exceptions import ItemNotFound
 
 def retrieve_by_id(table, id, response):
 
-	try:
-		item = table.get_item(id=id)
-		itemdb = table.scan()
-		response.status = 200
+  try:
+    item = table.get_item(id=id)
+    itemdb = table.scan()
+    response.status = 200
 
-		for i in itemdb:
+    for i in itemdb:
+      activityList = str(i["activities"])
+      if activityList == "None":
+      activityList = []
 
-			activityList = str(i["activities"])
+      return {"data": {
+        "type": "person",
+        "id": id,
+        "name": item['name'],
+        "activities": activityList
+        }
+      }
 
-			if activityList == "None":
-				activityList = []
-
-			return {"data": {
-						"type": "person",
-						"id": id,
-						"name": item['name'],
-						"activities": activityList
-					}
-				}
-		
-	except ItemNotFound as inf:
-		response.status = 404
-		return {"errors": [{
-					"not_found": {
-						"id": id
-					}
-				}]
-			}
+  except ItemNotFound as inf:
+    response.status = 404
+    return {"errors": [{
+      "not_found": {
+        "id": id
+        }
+      }]
+    }
