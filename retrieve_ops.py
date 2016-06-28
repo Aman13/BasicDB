0f923ffc467dbf854a8684c8831a889ef1f7f257
generiglesias <giglesia@sfu.ca>
2016-06-28 14:37:17 -0700

    Implement retrieve by id function.
    Added get functionality for each user by id.
    Converted list of activities. Had to convert to string first.

--- retrieve_ops.py
+++ retrieve_ops.py
@@ -1,8 +1,30 @@
 ''' Retrieve operations for CMPT 474 Assignment 2 '''
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.exceptions import ItemNotFound
 
 def retrieve_by_id(table, id, response):
-    print "Retrieve by id not yet implemented"
-    response.status = 501
+  try:
+    item = table.get_item(id=id)
+    itemdb = table.scan()
+    response.status = 200
+
+    for i in itemdb:
+
+      activityList = str(i["activities"])
+
+      if activityList == "None":
+        activityList = []
+
+      return {"data": {
+        "type": "person",
+        "id": id,
+        "name": item['name'],
+        "activities": activityList
+        }
+      }
+
+  except ItemNotFound as inf:
+    response.status = 404
     return {"errors": [{
         "retrieve by id not implemented": {"id": id}
         }]}
