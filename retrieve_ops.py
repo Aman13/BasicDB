3c3cd00838959253a6cc5ad614be31228337657f
generiglesias <giglesia@sfu.ca>
2016-06-28 23:29:37 -0700

    Implement retrieve user by name and change frontend.py.
    🙏Praise Aman🙏

--- retrieve_ops.py
+++ retrieve_ops.py
@@ -31,3 +31,39 @@ def retrieve_by_id(table, id, response):
     return {"errors": [{
         "retrieve by id not implemented": {"id": id}
         }]}
+
+def retrieve_by_name(table, name, response):
+
+    itemdb = table.scan()
+    for i in itemdb:
+        iname = str(i["name"])
+
+        if iname==name:
+            itemid = int(i["id"]) 
+            response.status = 200 # Found
+
+            if i['activities'] == None:
+              return {"data": {
+                      "type": "person",
+                      "id": itemid,
+                      "name": name,
+                      "activities": []
+                      }
+                  }
+            else:
+              activityList = list(i["activities"])
+              return {"data": {
+                      "type": "person",
+                      "id": itemid,
+                      "name": name,
+                      "activities": activityList
+                      }
+                  }
+  
+    response.status = 404 #Not Found
+    return {"errors": [{
+                "not_found": {
+                    "name": name
+                }
+            }]
+        }
