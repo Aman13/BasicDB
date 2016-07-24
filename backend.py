4729fddb17372b75995cc83bc98c550aabb9d160
generiglesias <giglesia@sfu.ca>
2016-07-24 16:59:21 -0700

    Skeleton Assignment
    User @put command now sends command to backend.
    
    🙏Bless Up🙏

--- backend.py
+++ backend.py
@@ -24,6 +24,15 @@ MAX_TIME_S = 3600 # One hour
 MAX_WAIT_S = 20 # SQS sets max. of 20 s
 DEFAULT_VIS_TIMEOUT_S = 60
 
+def open_conn(region):
+    conn = boto.sqs.connect_to_region(AWS_REGION)
+    if conn == None:
+        sys.stderr.write("Could not connect to AWS region '{0}'\n".format(AWS_REGION))
+        sys.exit(1)
+    return conn
+
+def open_q(name, conn):
+    return conn.create_queue(name)
 
 def handle_args():
     argp = argparse.ArgumentParser(
@@ -50,3 +59,23 @@ if __name__ == "__main__":
             * record the message id and response
             * put the response on the output queue
     '''
+    global table
+    try:
+        conn = boto.dynamodb2.connect_to_region(AWS_REGION)
+        if conn == None:
+            sys.stderr.write("Could not connect to AWS region '{0}'\n".format(AWS_REGION))
+            sys.exit(1)
+
+        table = boto.dynamodb2.table.Table(TABLE_NAME_BASE+sys.argv[1], connection=conn)
+    except Exception as e:
+        sys.stderr.write("Exception connecting to DynamoDB table {0}\n".format(TABLE_NAME_BASE+sys.argv[1]))
+        sys.stderr.write(str(e))
+        sys.exit(1)
+
+    conn = open_conn(AWS_REGION)
+    q_in = open_q("a3_back_in_b", conn)
+    while True: 
+      rs = q_in.get_messages()
+      m = rs[0]
+      print m.get_body()
+        
